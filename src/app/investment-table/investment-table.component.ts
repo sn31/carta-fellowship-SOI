aa0a2e2c0431197afb8599b9b371597076ef7959
SkyeN <skye@dames.es>
2018-11-19 19:07:46 -0800

    add bootstrap table

---
+++ src/app/investment-table/investment-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-table',
+  templateUrl: './investment-table.component.html',
+  styleUrls: ['./investment-table.component.css']
+})
+export class InvestmentTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
