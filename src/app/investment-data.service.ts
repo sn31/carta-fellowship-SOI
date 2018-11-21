636d607a74e5b1d5757e6a6553426599b1074a67
SkyeN <skye@dames.es>
2018-11-20 19:44:08 -0800

    add date input to API call

--- src/app/investment-data.service.ts
+++ src/app/investment-data.service.ts
@@ -8,6 +8,6 @@ export class InvestmentDataService {
   constructor(private http: Http) { }
 
   getByDate(date: string) {
-   return this.http.get(`https://gist.githubusercontent.com/cranium/d8b83184bf0750f2c834760b7c9203dc/raw/a73a70716951f77b90e84b8848ff1fee46938dd1/?${date}?soi.json`);
+   return this.http.get(`https://gist.githubusercontent.com/cranium/d8b83184bf0750f2c834760b7c9203dc/raw/a73a70716951f77b90e84b8848ff1fee46938dd1/soi.json?date=${date}`);
   }
 }
