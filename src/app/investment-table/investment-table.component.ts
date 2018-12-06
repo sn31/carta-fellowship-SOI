df462163f45016f45e4f7a3ed66af8615c3fb8ca
SkyeN <skye@dames.es>
2018-12-05 17:52:34 -0800

    create show/hide status dictionary for each investment

--- src/app/investment-table/investment-table.component.ts
+++ src/app/investment-table/investment-table.component.ts
@@ -1,28 +1,32 @@
-import { Component} from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import {InvestmentDataService} from '../investment-data.service';
-
+import { Component } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { InvestmentDataService } from "../investment-data.service";
 
 @Component({
-  selector: 'app-investment-table',
-  templateUrl: './investment-table.component.html',
-  styleUrls: ['./investment-table.component.css'],
+  selector: "app-investment-table",
+  templateUrl: "./investment-table.component.html",
+  styleUrls: ["./investment-table.component.css"],
   providers: [InvestmentDataService]
 })
 export class InvestmentTableComponent {
   investments: any[];
   investmentTotal: number = 0;
   show: boolean = false;
+  
+  showInvestmentDict = {};
   constructor(private investmentDataService: InvestmentDataService) {
     this.getInvestmentData(this.date); //Show data on defaul date.
-   }
-  getInvestmentData(date:string){
+  }
+  getInvestmentData(date: string) {
     this.investmentDataService.getByDate(date).subscribe(response => {
       this.investments = response.json();
-      this.investments.map(investment=>
-        this.investmentTotal+= investment["cost"]["$"],
-      )
-    })
+      this.investments.map(
+        investment => (
+          this.showInvestmentDict[investment["name"]]= false,
+          this.investmentTotal += investment["cost"]["$"]
+        )
+      );
+    });
   }
 
   date: string = "2018-03-12";
@@ -31,5 +35,10 @@ export class InvestmentTableComponent {
     this.investmentTotal = 0;
     this.getInvestmentData(this.date);
     console.log("Reloading the table");
+  }
+  openDetails() {
+    console.log(this.showInvestmentDict);
+    // this.showInvestment = !this.showInvestment;
+    // console.log(this.showInvestment);
   }
 }
